7403b1979f4e4d99b57041f31b77fb66527c7d56
Rifqi Wibisono <rifqiwibisono271002@gmail.com>
2026-03-08 13:04:06 +0800

    Grouping player class

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ def main():
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     dt = 0
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    
     player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
     
     while True:
@@ -18,8 +23,9 @@ def main():
                 return
             
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
     
